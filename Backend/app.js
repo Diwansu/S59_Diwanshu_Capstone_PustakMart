bd10efaa671d0a67dcd126ffc76536f1a47206d8
Diwanshu Baskota <diwanshubaskota@Diwanshus-MacBook-Air-9.local>
2024-12-24 21:28:52 +0545

    Changes to the existing Codes and Update and delete concepts showed via Put and delete

--- Backend/app.js
+++ Backend/app.js
@@ -4,10 +4,15 @@ const app = express();
 require("dotenv").config();
 require("./connection/conn");
 const user = require("./routes/userRoutes");
-
+const Books = require("./routes/book");
+const Favourite = require("./routes/favourite");
+const Cart = require("./routes/cart");
 app.use(express.json());
 
 app.use("/ap1/v1", user) ;
+app.use("/api/v1" , Books);
+app.use("/api/v1" , Favourite);
+
 
 
 
